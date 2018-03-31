f40cd32616c9955f1d25033de5a956823935c80d
Paul Chen <paulchenpmc@gmail.com>
2018-03-30 23:33:31 -0600

    some global variables

--- AdaptiveTreeWalk.cpp
+++ AdaptiveTreeWalk.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
+#define NUM_STATIONS 1024  // N
+
+// Global variables
+int bus[NUM_STATIONS];
+int readyStations; // K
+int probeLevel; // I
+
+////////////////////////////////////////////////////////////////////////////////
+// Functions
+
+void generateRandomFrames() {
+  for (int i = 0; i < NUM_STATIONS; i++)
+    bus[NUM_STATIONS] = rand() % 2;
+}
+
+
+
+// End of functions
+////////////////////////////////////////////////////////////////////////////////
 int main() {
-  cout << "Hello world" << endl;
+  cout << "Starting simulation..." << endl;
+  srand(time(NULL)); // Makes rand() more random
+  generateFrames();
+
 }
