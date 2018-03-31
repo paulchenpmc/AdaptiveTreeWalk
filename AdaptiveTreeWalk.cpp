bbe6f0902fa0cbeb90d1ce6df2204d7040accd8a
Paul Chen <paulchenpmc@gmail.com>
2018-03-31 10:46:52 -0600

    basic frame of simulation laid out. there is a bug in generateRandomFrames function that causes infinite loop though.

--- AdaptiveTreeWalk.cpp
+++ AdaptiveTreeWalk.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 #define NUM_STATIONS 8  // N
 
 // Global variables
 int stationsArr[NUM_STATIONS];
-int readyStations; // K
-int probeLevel; // I
+int numReadyStations[] = {1,2,4,8,16,32,64,128,256,512,1024};  // K
+int numReadyStationsN = 11;
+int probeLevels[] = {0,2,4,6,8,10};  // I
+int probeLevelsN = 6;
 int numProbes;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
 
+// Randomly readies k stations for sending
 void generateRandomFrames(int k) {
   // Set all stations to 0
   for (int i = 0; i < NUM_STATIONS; i++)
     stationsArr[i] = 0;
 
+  // Randomly ready k stations
   // int numLoops = 0;
   int numReady = 0;
   for (int i = 0; true; i++) {
@@ -32,19 +37,34 @@ void generateRandomFrames(int k) {
   //   cout << stationsArr[i] << endl;
 }
 
-int probeTree() {
+// Probes tree and tracks: collisions, # probes, # idle
+int probeTree(int startingLevel) {
 
 }
 
 void runSimulation() {
-
+  // For each different k stations ready
+  for (int a = 0; a < numReadyStationsN; a++) {
+    int k = numReadyStations[a];
+    printf("k=%d\n", k);
+    // 100 random combinations of k stations
+    for (int b = 0; b < 100; b++) {
+      if (b%10==0) printf("  test #%d\n", b);
+      generateRandomFrames(k);
+      // Probe starting on different levels i
+      for (int c = 0; c < probeLevelsN; c++) {
+        int startingLevel = probeLevels[c];
+        probeTree(startingLevel);
+      }
+    }
+  }
 }
 
 // End of functions
 ////////////////////////////////////////////////////////////////////////////////
+
 int main() {
   cout << "Starting simulation..." << endl;
   srand(time(NULL)); // Makes rand() more random
-  generateRandomFrames(0);
   runSimulation();
 }
